1fb8f0c5a0146ce859670ae9e5c17e83c7b09bbd
Try Ajitiono <ballinst@gmail.com>
2022-04-05 14:49:08 +0700

    cp

--- src/helpers/header-groups.test.ts
+++ src/helpers/header-groups.test.ts
@@ -2,13 +2,18 @@ import { processHeaderGroups, getHeaderIdxMap } from './header-groups';
 import { TableColumnType } from './types';
 
 interface Values {
+  name: string;
   q1: number;
   q2: number;
   q3: number;
   q4: number;
 }
 
-const TABLE_HEADERS: TableColumnType<any>[] = [
+const TABLE_HEADERS: TableColumnType<Values>[] = [
+  {
+    prop: 'name',
+    title: 'Name'
+  },
   {
     prop: 'q1',
     title: 'Quarter 1'
@@ -30,10 +35,11 @@ const TABLE_HEADERS: TableColumnType<any>[] = [
 test('getHeaderIdxMap', () => {
   const headerPropIdxMap = getHeaderIdxMap(TABLE_HEADERS);
   expect(headerPropIdxMap).toEqual({
-    q1: 0,
-    q2: 1,
-    q3: 2,
-    q4: 3
+    name: 0,
+    q1: 1,
+    q2: 2,
+    q3: 3,
+    q4: 4
   });
 });
 
@@ -42,9 +48,16 @@ describe('processHeaderGroups', () => {
 
   test('one fold', () => {
     expect(
-      processHeaderGroups(TABLE_HEADERS, {
-        'First Semester': ['']
-      })
+      processHeaderGroups(TABLE_HEADERS, [
+        {
+          title: 'First Semester',
+          children: ['q1', 'q2']
+        },
+        {
+          title: 'Second Semester',
+          children: ['q3', 'q4']
+        }
+      ])
     );
   });
 });
--- src/helpers/header-groups.ts
+++ src/helpers/header-groups.ts
@@ -3,18 +3,22 @@ import { TableColumnType } from './types';
 // TODO(imballinst): for v4, we need to refactor the table and make this mandatory,
 // Default the type to `any` so it's not mandatory.
 // so that the grouped table headers have stronger typings.
-export interface TableHeaderGroups<T extends Record<string, any> = any> {
-  [index: string]: (keyof T)[] | TableHeaderGroups;
+export interface TableHeaderGroup<
+  TTableColumnType extends Record<string, any> = any
+> {
+  title: string;
+  children: (keyof TTableColumnType | TableHeaderGroup<TTableColumnType>)[];
 }
 
-export function processHeaderGroups(
-  headers: TableColumnType<any>[],
-  headerGroups?: TableHeaderGroups
+export function processHeaderGroups<TTableColumnType>(
+  headers: TableColumnType<TTableColumnType>[],
+  headerGroups?: TableHeaderGroup<TTableColumnType>[]
 ) {
   const headerPropToIdxMap = getHeaderIdxMap(headers);
 }
 
-export function getHeaderIdxMap(headers: TableColumnType<any>[]) {
+// Helper functions.
+function getHeaderIdxMap(headers: TableColumnType<any>[]) {
   const headerPropToIdxMap: Record<string, number> = {};
   for (let i = 0; i < headers.length; i += 1) {
     headerPropToIdxMap[headers[i].prop as string] = i;
@@ -23,10 +27,36 @@ export function getHeaderIdxMap(headers: TableColumnType<any>[]) {
   return headerPropToIdxMap;
 }
 
-function extractRanges(
-  headers: TableColumnType<any>[],
-  headerGroups: TableHeaderGroups,
-  headerPropToIdxMap: Record<string, number>
-) {
+function getNestedHeaders(
+  headerPropToIdxMap: Record<string, number>,
+  headerGroups: TableHeaderGroup[],
+  deepestLevel = 0
+): {
+  nestedHeaders: string[];
+  deepestLevel: number;
+} {
   //
+  const nestedHeaders: string[] = [];
+  let deepest = deepestLevel;
+
+  for (const headerGroup of headerGroups) {
+    for (const child of headerGroup.children) {
+      if (typeof child !== 'object') {
+        nestedHeaders.push(String(child));
+      } else {
+        const result = getNestedHeaders(
+          headerPropToIdxMap,
+          headerGroups,
+          deepest
+        );
+        nestedHeaders.push(...result.nestedHeaders);
+
+        if (result.deepestLevel > deepest) {
+          deepest = result.deepestLevel;
+        }
+      }
+    }
+  }
+
+  return { nestedHeaders, deepestLevel: deepest };
 }
